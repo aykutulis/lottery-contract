e3f2758a17ccab36285f8f654c95a869ad7214d5
Aykut Ulış <aykutulis@hotmail.com>
2022-03-28 02:07:17 +0300

    config updated, test created

--- src/deploy.ts
+++ src/deploy.ts
@@ -3,23 +3,12 @@ import HDWalletProvider from '@truffle/hdwallet-provider';
 import Web3 from 'web3';
 import { abi, evm } from './compile';
 
-const { MNEMONIC_PHRASE, RINKEBY_URI, INITIAL_CONTRACT_ARGS } = process.env;
+const { MNEMONIC_PHRASE, RINKEBY_URI } = process.env;
 
 // validate environment variables
 if (!MNEMONIC_PHRASE || !RINKEBY_URI) {
   throw new Error('Please set the "MNEMONIC_PHRASE" and "RINKEBY_URI" environment variables');
 }
-if (!INITIAL_CONTRACT_ARGS) {
-  throw new Error('Please set the "INITIAL_CONTRACT_ARGS" environment variable');
-}
-
-const initialContractArgs: string[] = (() => {
-  try {
-    return JSON.parse(INITIAL_CONTRACT_ARGS);
-  } catch (e) {
-    throw new Error('"INITIAL_CONTRACT_ARGS" environment variable must be a JSON array');
-  }
-})();
 
 // create a provider
 const provider = new HDWalletProvider(MNEMONIC_PHRASE, RINKEBY_URI);
@@ -33,7 +22,7 @@ const web3 = new Web3(provider);
   console.info('Attempting to deploy contract from account', account);
 
   const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: initialContractArgs })
+    .deploy({ data: evm.bytecode.object })
     .send({ from: account, gas: 1000000 });
 
   console.info('Contract deployed to', result.options.address);
---
+++ src/test/Lottery.test.ts
@@ -0,0 +1,95 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { abi, evm } = require('../compile');
+
+let accounts: string[] = [];
+let lottery: InstanceType<typeof web3.eth.Contract>;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ from: accounts[0], gas: 1000000 });
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether'),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+
+  it('allows multiple accounts to enter', async () => {
+    const accountIndexes = [0, 1, 2, 3, 4];
+    for (const index of accountIndexes) {
+      await lottery.methods.enter().send({
+        from: accounts[index],
+        value: web3.utils.toWei('0.02', 'ether'),
+      });
+    }
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    for (const index of accountIndexes) {
+      assert.equal(accounts[index], players[index]);
+    }
+
+    assert.equal(accountIndexes.length, players.length);
+  });
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0,
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('sends money to the winner and resets the players array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether'),
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+  });
+});
