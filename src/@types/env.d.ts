a44d4f4b0dae8f8e44931da4d907fc04f6d80e4b
Aykut Ulış <aykutulis@hotmail.com>
2022-03-28 02:58:59 +0300

    gas amount env

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     MNEMONIC_PHRASE?: string;
     RINKEBY_URI?: string;
+    GAS_AMOUNT?: string;
   }
 }
--- src/deploy.ts
+++ src/deploy.ts
@@ -3,11 +3,14 @@ import HDWalletProvider from '@truffle/hdwallet-provider';
 import Web3 from 'web3';
 import { abi, evm } from './compile';
 
-const { MNEMONIC_PHRASE, RINKEBY_URI } = process.env;
+const { MNEMONIC_PHRASE, RINKEBY_URI, GAS_AMOUNT } = process.env;
 
 // validate environment variables
-if (!MNEMONIC_PHRASE || !RINKEBY_URI) {
-  throw new Error('Please set the "MNEMONIC_PHRASE" and "RINKEBY_URI" environment variables');
+if (!MNEMONIC_PHRASE || !RINKEBY_URI || !GAS_AMOUNT) {
+  throw new Error('Please set the "MNEMONIC_PHRASE", "RINKEBY_URI" and "GAS_AMOUNT" environment variables');
+}
+if (isNaN(parseFloat(GAS_AMOUNT))) {
+  throw new Error('Please set the "GAS_AMOUNT" environment variable as a number');
 }
 
 // create a provider
@@ -23,7 +26,7 @@ const web3 = new Web3(provider);
 
   const result = await new web3.eth.Contract(abi)
     .deploy({ data: evm.bytecode.object })
-    .send({ from: account, gas: 1000000 });
+    .send({ from: account, gas: parseFloat(GAS_AMOUNT) });
 
   console.info('Contract deployed to', result.options.address);
   provider.engine.stop();
