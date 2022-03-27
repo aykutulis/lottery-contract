03ddfd06fa492444f604bdda2607daa47312f97d
Aykut Ulış <aykutulis@hotmail.com>
2022-03-28 02:50:54 +0300

    contract updated

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] public players;
 
     constructor() {
         manager = msg.sender;
@@ -11,7 +11,7 @@ contract Lottery {
 
     function enter() public payable {
         require(msg.value > .01 ether);
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
 
     function random() public view returns (uint256) {
@@ -31,10 +31,10 @@ contract Lottery {
     function pickWinner() public restricted {
         uint256 index = random() % players.length;
         payable(players[index]).transfer(address(this).balance);
-        players = new address[](0);
+        players = new address payable[](0);
     }
 
-    function getPlayers() public view returns (address[] memory) {
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
 }
